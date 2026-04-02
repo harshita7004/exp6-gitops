b3ad71f452df192a7a66d2e09957af34b05b5895
mn@gmail.com <harshitasingh7004@gmail.com>
2026-04-02 15:20:48 +0530

    trigger ci

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello from CI/CD GitOps 🚀');
+  res.send("Updated by CI/CD 🚀🔥");
 });
 
 app.listen(3000, () => {
